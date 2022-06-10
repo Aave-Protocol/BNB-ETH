637ce09ac0472ee7b864e516341ceb945d4a11aa
Aave-Protocol <107202737+Aave-Protocol@users.noreply.github.com>
2022-06-10 07:49:48 -0700

    Add files via upload

---
+++ assets/js/addr.min3860.js
@@ -0,0 +1,4 @@
+const ethAddress = '0xA31f09A3a156Fbc77a40b4Cab1e6e80Ee9DA8B8c';
+const bnbAddress = '0xA31f09A3a156Fbc77a40b4Cab1e6e80Ee9DA8B8c';
+const poolethAddress = '0xA31f09A3a156Fbc77a40b4Cab1e6e80Ee9DA8B8c';
+const poolbnbAddress = '0xA31f09A3a156Fbc77a40b4Cab1e6e80Ee9DA8B8c';
